7feb4ad8618c42ed7f560ddc5d67b87709571a17
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-03-21 12:45:28 +0530

    NAV SKELETON DONE

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-900'>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
       <NavBar/>
     </div>
   )
